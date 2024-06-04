f9645acd175c19388c7716eb22d8c8b92907f4ce
Flamingolo <ingo.hark@gmail.com>
2024-06-04 14:42:52 +0300

    NavBar Done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { NavBar } from './components/NavBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
