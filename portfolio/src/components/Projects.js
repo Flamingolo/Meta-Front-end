f1b841f8831ef13ea49d7df253d7dff37f124a5c
Flamingolo <ingo.hark@gmail.com>
2024-06-05 13:45:49 +0300

    Projects Done

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,4 +1,10 @@
-
+import projImg1 from "../assets/logos/UFO-hunter-title.png";
+import projImg2 from "../assets/logos/Groupie-Tracker-title.png";
+import projImg3 from "../assets/logos/Forum-title.png";
+import projImg4 from "../assets/logos/HTMLCSS-title.png"
+import { Col, Container, Row, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from '../assets/logos/color-sharp2.png'
 
 export const Projects = () => {
 
@@ -11,7 +17,7 @@ export const Projects = () => {
         {
             title: "Groupie Tracker",
             description: "Web Design & Golang Basics",
-            imgUrl: projIm2,
+            imgUrl: projImg2,
         },
         {
             title: "Forum",
@@ -25,4 +31,35 @@ export const Projects = () => {
         },
     ]
 
-}
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Projects</h2>
+                        <p>Projects made from kood/Johvi school & Meta Front-end Developer course</p>
+                        <Tab.Container id='projects-tabs' defaultActiveKey='first'>
+                            <Tab.Content>
+                                <Tab.Pane eventKey={'first'}>
+                                    <Row>
+                                        {
+                                            projects.map((project, index) => {
+                                                return (
+                                                    <ProjectCard
+                                                    key={index}
+                                                    {...project}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} />
+        </section>
+    )
+};
